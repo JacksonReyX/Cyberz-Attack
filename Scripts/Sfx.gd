42f5315a27ff931c14c92f9b1379072f4b471cb5
KikiMiley <185963707+Dantekam@users.noreply.github.com>
2026-03-09 13:12:58 -0400

    sign
    sign

--- Scripts/Sfx.gd
+++ Scripts/Sfx.gd
@@ -30,5 +30,9 @@ func play(stream: AudioStream) -> void:
 	p.play()
 
 func chest() -> void:
+	print("[Sfx] chest() called. chest_stream =", chest_stream)
+	play(chest_stream)
+	
+func sign() -> void:
 	print("[Sfx] sign() called. sign_stream =", sign_stream)
 	play(sign_stream)
