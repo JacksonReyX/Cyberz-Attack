1ad8626943b46a8812c60221bb44ccf21625f20d
SabellaMalisher <malisher.sabella@gmail.com>
2026-03-10 14:20:23 -0400

    key

--- scripts/key.gd
+++ scripts/key.gd
@@ -1,6 +1,9 @@
 extends Area2D
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var key_icon: TextureRect = $"../../CanvasLayer/KeyHUD/KeyIcon"
 
 func _on_body_entered(body: Node2D) -> void:
 	print("key found")
-	queue_free()
+	animation_player.play("pickup")
+	key_icon.visible = true
