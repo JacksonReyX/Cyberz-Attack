3875221e1b2932277feff48568b075dd30ed7ee6
SabellaMalisher <malisher.sabella@gmail.com>
2026-03-16 16:08:41 -0400

    Lock Doors

--- Scripts/game_state.gd
+++ Scripts/game_state.gd
@@ -2,3 +2,4 @@ extends Node
 
 var coins: int = 0
 var health_potions = 0
+var keys: int = 0
--- scripts/key.gd
+++ scripts/key.gd
@@ -2,8 +2,12 @@ extends Area2D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var key_icon: TextureRect = $"../../CanvasLayer/KeyHUD/KeyIcon"
+@onready var key_hud: Control = $"../../CanvasLayer/KeyHUD"
 
 func _on_body_entered(body: Node2D) -> void:
 	print("key found")
+	GameState.keys += 1
 	animation_player.play("pickup")
 	key_icon.visible = true
+	
+	key_hud.show_key_message()
