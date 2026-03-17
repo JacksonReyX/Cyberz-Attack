64a8aeeaff101756eb5f71d3a86c7de4186c4004
SabellaMalisher <malisher.sabella@gmail.com>
2026-03-17 14:36:40 -0400

    locked door

--- scripts/locked_door.gd
+++ scripts/locked_door.gd
@@ -3,12 +3,14 @@ extends Area2D
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var prompt: Sprite2D = $buttonPrompt
 @onready var collision_shape_2d: CollisionShape2D = $StaticBody2D/CollisionShape2D
+@onready var locked_label: Label = $LockedLabel
 
 
 var player_in_range = false
 
 func _ready():
 	prompt.visible = false 
+	locked_label.visible = false
 	body_entered.connect(_on_body_entered)
 	body_exited.connect(_on_body_exited)
 
@@ -19,15 +21,19 @@ func _process(_delta):
 			sprite.play("open")
 			collision_shape_2d.disabled = true
 			prompt.visible = false
+			locked_label.visible = false
 		else:
-			print("Find key to unlock")
+			locked_label.visible = true
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
 		player_in_range = true
 		prompt.visible = true
+		if GameState.keys == 0:
+			locked_label.visible = true
 
 func _on_body_exited(body):
 	if body.is_in_group("player"):
 		player_in_range = false
 		prompt.visible = false
+		locked_label.visible = false
