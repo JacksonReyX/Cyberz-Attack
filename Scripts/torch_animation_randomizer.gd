f6dfe89e49d143512b1d9e646f7a61dc5441be37
SabellaMalisher <malisher.sabella@gmail.com>
2026-04-19 11:42:55 -0400

    level 2 layout

--- Scripts/torch_animation_randomizer.gd
+++ Scripts/torch_animation_randomizer.gd
@@ -2,7 +2,11 @@ extends StaticBody2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 const animations = ["animation1", "animation2"]
+@onready var light: PointLight2D = $PointLight2D
 
 
 func _ready() -> void:
 	animated_sprite_2d.play(animations.pick_random())
+
+func disable_torch():
+	light.visible = false
