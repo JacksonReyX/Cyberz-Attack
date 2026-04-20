574112d3c770faa5a934e16ba6c91e8812cfadd8
KikiMiley <185963707+Dantekam@users.noreply.github.com>
2026-04-20 08:38:17 -0400

    Big Changes
    Finished animation, coloring, and cursor.

--- Scripts/player_customization.gd
+++ Scripts/player_customization.gd
@@ -1,3 +1,10 @@
 extends Node
 
-var selected_class := "Knight" # "Knight" or "Wizard"
+var selected_class: String = "Knight"
+
+# Customization colors
+var color_outfit: Color = Color.WHITE
+var color_accessory: Color = Color.WHITE
+var color_body: Color = Color.WHITE
+var color_eyes: Color = Color.WHITE
+var color_hair: Color = Color.WHITE
